f9a2abcbb7affe9d6a34610679dfd8f9b20fc04d
AhmedMahmoud3030 <ahmed.elbeah@icloud.com>
2022-04-14 11:08:44 +0200

    ss

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //
       title: 'IExpenses',
       theme: ThemeData(
           primarySwatch: Colors.purple,
